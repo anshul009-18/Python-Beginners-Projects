9b00b38466ca941d3132d65ae611e68c098d6a0f
Anshul Joshi <126666276+anshul009-18@users.noreply.github.com>
2025-01-26 15:52:35 +0530

    Add files via upload

---
+++ Hangman game.py
@@ -0,0 +1,165 @@
+import random 
+
+from logging import PlaceHolder
+
+words_list = [
+    "apple", "banana", "chair", "python", "coffee", "window", "flower", "table", "water", "orange",
+    "kitten", "laptop", "mountain", "bottle", "cookie", "garden", "river", "sunset", "mirror", "camera",
+    "pencil", "monkey", "desert", "forest", "music", "puzzle", "rocket", "balloon", "planet", "castle",
+    "bridge", "island", "diamond", "shadow", "dragon", "candle", "tiger", "thunder", "pillow", "castle",
+    "violin", "glacier", "rainbow", "helmet", "jungle", "carpet", "crystal", "giraffe", "volcano", "zipper",
+    "elephant", "notebook", "bracelet", "scarf", "popcorn", "umbrella", "bicycle", "backpack", "sandwich", "penguin",
+    "mushroom", "galaxy", "clock", "airplane", "rainbow", "treasure", "compass", "glasses", "bookshelf", "dolphin",
+    "butterfly", "telescope", "horizon", "coral", "sunflower", "hammock", "cactus", "meadow", "lantern", "spiral",
+    "anchor", "waterfall", "pirate", "compass", "mermaid", "submarine", "explorer", "seagull", "reef", "icicle",
+    "otter", "fireplace", "sled", "igloo", "polar", "aurora", "cocoa", "iceberg", "mitten", "sleigh",
+    "rainstorm", "cupcake", "pyramid", "zombie", "penguin", "volcano", "avocado", "squirrel", "galaxy", "parrot",
+    "butterfly", "guitar", "dinosaur", "spaceship", "cloud", "whale", "sunrise", "tornado", "trolley", "puzzle",
+    "snowman", "clouds", "highway", "turtle", "waterfall", "painting", "starfish", "mystery", "dragonfly", "rainbow",
+    "unicorn", "mermaid", "seahorse", "castle", "lighthouse", "reindeer", "compass", "mountain", "snowflake", "raccoon",
+    "moonlight", "crystal", "puzzle", "balloon", "paradise", "mango", "oceans", "lightning", "trolley", "chocolate",
+    "storm", "mountain", "mountain", "cloud", "carpet", "diamond", "orchid", "volcano", "pirate", "breeze",
+    "horizon", "rider", "thunder", "explorer", "treasure", "exploration", "keyboard", "telescope", "island", "rocket",
+    "scarf", "storm", "fireplace", "zebra", "trolley", "flamingo", "bicycle", "sandwich", "telescope", "dream",
+    "glasses", "cherry", "polar", "sunflower", "dolphin", "snowball", "igloo", "umbrella", "pillow", "space",
+    "kitchen", "ocean", "vacation", "rain", "mountain", "bubble", "cactus", "butterfly", "parrot", "hedgehog",
+    "jungle", "clothesline", "camera", "sunshine", "icecream", "puzzle", "planets", "firefly", "breeze", "pineapple",
+    "oasis", "scissors", "breeze", "peach", "elephant", "lighthouse", "skyscraper", "balloon", "piano", "lighthouse"
+]
+
+
+# Hangman stages
+hangman_stages = [
+    """
+     ----- 
+     |   | 
+     O   | 
+    /|\\  | 
+     |   | 
+    /    | 
+    """, 
+    
+    """
+     ----- 
+     |   | 
+     O   | 
+    /|\\  | 
+     |   | 
+         | 
+    """,  
+
+    """
+     ----- 
+     |   | 
+     O   | 
+    /|   | 
+     |   | 
+         | 
+    """,  
+
+    """
+     ----- 
+     |   | 
+     O   | 
+     |   | 
+     |   | 
+         | 
+    """,  
+
+    """
+     ----- 
+     |   | 
+     O   | 
+         | 
+         | 
+         | 
+    """,  
+
+    """
+     ----- 
+     |   | 
+     O   | 
+         | 
+         | 
+         | 
+    """,  
+
+    """
+     ----- 
+     |   | 
+         | 
+         | 
+         | 
+         | 
+    """   
+]
+
+
+logo = ''' 
+██╗░░██╗░█████╗░███╗░░██╗░██████╗░███╗░░░███╗░█████╗░███╗░░██╗
+██║░░██║██╔══██╗████╗░██║██╔════╝░████╗░████║██╔══██╗████╗░██║
+███████║███████║██╔██╗██║██║░░██╗░██╔████╔██║███████║██╔██╗██║
+██╔══██║██╔══██║██║╚████║██║░░╚██╗██║╚██╔╝██║██╔══██║██║╚████║
+██║░░██║██║░░██║██║░╚███║╚██████╔╝██║░╚═╝░██║██║░░██║██║░╚███║
+╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝░╚═════╝░╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝
+''' 
+
+
+print(logo)
+
+
+lives = 6
+
+# Generate random word from list
+word = random.choice(words_list)
+print(word)
+
+
+# Create empty space 
+PlaceHolder = ""
+
+for position in range(len(word)):
+    PlaceHolder += "_ "
+print(PlaceHolder)
+
+game_over = False
+
+correct_letter = []
+
+while not game_over:
+    guess= input('Guess the letter: ').lower() 
+
+    display = ""
+
+    # Checking the letter in word or not
+    for letter in word:
+        
+        if letter == guess:
+            display += letter
+            correct_letter.append(guess)
+            
+        elif letter in correct_letter:
+            display += letter
+            
+        else:
+            display += "_ "
+
+    print(display)
+    
+    if guess not in word:
+        lives -= 1
+        if lives ==  0:
+            game_over = True 
+            print("You Lose ")
+            print(f"The word was {word}")
+    
+    if "_ " not in display:
+        game_over = True
+        print("YOU WIN ")
+        
+        
+    print(hangman_stages[lives])
+    
+    
+
+
